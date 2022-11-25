25b591ed5e85f81d3549fe85561cbddfc3e26eef
bartvanbenthem <bartvanbenthem@hotmail.com>
2022-11-25 15:47:55 +0100

    organize main

--- operator/main.go
+++ operator/main.go
@@ -47,8 +47,8 @@ var (
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-	utilruntime.Must(apiv2.AddToScheme(scheme))
 	utilruntime.Must(cnadv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(apiv2.AddToScheme(scheme))
 	utilruntime.Must(kedav2.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
